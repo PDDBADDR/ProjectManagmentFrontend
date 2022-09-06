d61a228ec01ee75bb019e7c6b7e850ec3c66dcea
Adrian Podbielski <addrpddb99@gmail.com>
2022-09-06 16:35:14 +0200

    Add basic theme files

---
+++ src/themes/dark.ts
@@ -0,0 +1,7 @@
+const theme = {
+    primary: '#aeb8f5',
+    secondary: '#808080',
+    bgPrimary: '#222',
+}
+
+export default theme;
---
+++ src/themes/light.ts
@@ -0,0 +1,7 @@
+const theme = {
+    primary: '#aeb8f5',
+    secondary: '#808080',
+    bgPrimary: '#fff',
+}
+
+export default theme;
