0b0c955238bf948a2693844ff34b772e6fb72987
Adrian Podbielski <addrpddb99@gmail.com>
2022-09-20 13:28:24 +0200

    Modify theme files

--- src/themes/dark.ts
+++ src/themes/dark.ts
@@ -1,7 +1,11 @@
-const theme = {
+const theme: theme = {
     primary: '#aeb8f5',
     secondary: '#808080',
-    bgPrimary: '#222',
+    muted: '#444',
+    dark: '#111',
+    bgPrimary: '#1E1134',
+    bgSecondary: '#160C28',
+    contrast: 'white'
 }
 
 export default theme;
