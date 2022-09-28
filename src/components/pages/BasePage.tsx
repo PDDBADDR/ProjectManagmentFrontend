227da8f8b7992bb00ecb03b9cc94cc17eae8d2ec
Adrian Podbielski <addrpddb99@gmail.com>
2022-09-28 23:39:50 +0200

    Modify base page navbar

--- src/components/pages/BasePage.tsx
+++ src/components/pages/BasePage.tsx
@@ -4,22 +4,37 @@ import Logo from '../../../public/logo.svg'
 import { Button } from '../UI/Buttons'
 import styled from 'styled-components'
 import Container from '../UI/Container'
+import { useAppSelector } from '../../app/hooks'
+import Avatar from '../UI/Avatar'
 
 export default function BasePage() {
+  const user = useAppSelector((state) => state.user)
   return (
     <MainWrapper>
       <Navbar>
         <Navbar.Logo img={Logo} />
         <Navbar.Menu>
-          <Button href='index' buttonType='link'>
+          <Button to='index' buttonType='link'>
             Home
           </Button>
-          <Button href='lolo2' buttonType='link'>
+          <Button to='lolo2' buttonType='link'>
             Link 1
           </Button>
           <Button buttonType='link'>Link 2</Button>
           <Button buttonType='link'>Link 3</Button>
-          <Button buttonType='filled'>Sign in</Button>
+          {user.username && (
+            <>
+              <Button to='logout' buttonType='link'>
+                Logout
+              </Button>
+              <Avatar username={user.username} />
+            </>
+          )}
+          {!user.username && (
+            <Button to='login' buttonType='filled'>
+              Sign in
+            </Button>
+          )}
         </Navbar.Menu>
       </Navbar>
       <Container>
