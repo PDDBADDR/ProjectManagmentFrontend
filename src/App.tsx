5a33be41d65049b219039345d2f9e527088cf5a4
Adrian Podbielski <addrpddb99@gmail.com>
2022-09-28 23:31:47 +0200

    Configure toastify

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 import { useRoutes } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
 import AppContext from './context/AppContext'
 import { routes } from './routes'
 
 function App() {
-  return <AppContext>{useRoutes(routes)}</AppContext>
+  return (
+    <>
+      <AppContext>{useRoutes(routes)}</AppContext>
+      <ToastContainer
+        position='bottom-left'
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </>
+  )
 }
 
 export default App
