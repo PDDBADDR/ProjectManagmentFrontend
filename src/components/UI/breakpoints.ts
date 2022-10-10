1d947e2fa839bd49246119b5336ca532f1be0188
Adrian Podbielski <addrpddb99@gmail.com>
2022-10-10 15:54:06 +0200

    create table with screen sizes

--- src/components/UI/breakpoints.ts
+++ src/components/UI/breakpoints.ts
@@ -1,20 +1,31 @@
-const size = {
-    mobileS: '320px',
-    mobileM: '375px',
-    mobileL: '425px',
-    tablet: '768px',
-    laptop: '1024px',
-    laptopL: '1440px',
-    desktop: '2560px'
+export const sizeName = [
+  'mobileS',
+  'mobileM',
+  'mobileL',
+  'tablet',
+  'laptop',
+  'laptopL',
+  'desktop',
+] as const
+
+const size: { [T in typeof sizeName[number]]: string } = {
+  mobileS: '320px',
+  mobileM: '375px',
+  mobileL: '425px',
+  tablet: '768px',
+  laptop: '1024px',
+  laptopL: '1440px',
+  desktop: '2560px',
 }
 
-export const devices = {
-    mobileS: `(min-width: ${size.mobileS})`,
-    mobileM: `(min-width: ${size.mobileM})`,
-    mobileL: `(min-width: ${size.mobileL})`,
-    tablet: `(min-width: ${size.tablet})`,
-    laptop: `(min-width: ${size.laptop})`,
-    laptopL: `(min-width: ${size.laptopL})`,
-    desktop: `(min-width: ${size.desktop})`,
-    desktopL: `(min-width: ${size.desktop})`
+export type deviceType = { [T in typeof sizeName[number]]: string }
+
+export const devices: deviceType = {
+  mobileS: `(min-width: ${size.mobileS})`,
+  mobileM: `(min-width: ${size.mobileM})`,
+  mobileL: `(min-width: ${size.mobileL})`,
+  tablet: `(min-width: ${size.tablet})`,
+  laptop: `(min-width: ${size.laptop})`,
+  laptopL: `(min-width: ${size.laptopL})`,
+  desktop: `(min-width: ${size.desktop})`,
 }
