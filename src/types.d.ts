03732978164d30fb7347b40847eab5d411b510b5
Adrian Podbielski <addrpddb99@gmail.com>
2022-10-28 10:54:28 +0200

    Update global types

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,19 +1,65 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 const colorsList = [
-    'primary',
-    'secondary',
-    'muted',
-    'dark',
-    'bgPrimary',
-    'bgSecondary',
+  'primary',
+  'secondary',
+  'muted',
+  'dark',
+  'bgPrimary',
+  'bgSecondary',
+  'primaryFont',
 ] as const
 
-type color = typeof colorsList[number];
+type color = typeof colorsList[number]
 type themeColors = {
-    [T in typeof colorsList[number]]:string
+  [T in typeof colorsList[number]]: string
 }
 
-type contrast = 'white' | 'black';
+type contrast = 'white' | 'black'
 
 type theme = {
-    contrast: contrast;
-} & themeColors;
+  contrast: contrast
+} & themeColors
+
+interface Member {
+  email: string
+  id: number
+  username: string
+  is_active: boolean
+}
+
+interface Task {
+  name: string
+  description: string
+  index: number
+  project_id: number
+  status_id: number
+  id: number
+}
+
+interface Reindex {
+  id: number
+  index: number
+  [key: string]: any
+}
+
+type ReindexObject = 'TASK' | 'STATUS'
+
+interface Status {
+  name: string
+  index: number
+  project_id: number
+  tasks: Task[]
+  id: number
+}
+
+interface Project {
+  name: string
+  owner_id: number
+  id: number
+  tasks_count: number
+  members_count: number
+}
+
+interface ProjectStatusState extends Status {
+  droppableId: string
+}
